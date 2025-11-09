7f8f7bde03c991408393a22789fe63186aab877b
Pribavkin Andrei Alexeevich <155998621+wilsonSev@users.noreply.github.com>
2025-11-09 16:31:51 +0300

    Add files via upload

---
+++ LevelUp/LevelUpTests/LevelUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  LevelUpTests.swift
+//  LevelUpTests
+//
+//  Created by Андрей Прибавкин on 9.11.25.
+//
+
+import Testing
+@testable import LevelUp
+
+struct LevelUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
