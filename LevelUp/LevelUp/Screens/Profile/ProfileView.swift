b7a81eae4164a4d655ce0a660812066806beb7cc
Pribavkin Andrei Alexeevich <andrepribavkin@gmail.com>
2025-12-09 21:50:55 +0300

    Fix profile view style

--- LevelUp/LevelUp/Screens/Profile/ProfileView.swift
+++ LevelUp/LevelUp/Screens/Profile/ProfileView.swift
@@ -15,37 +15,44 @@ struct ProfileView: View {
     
     var body: some View {
         ZStack {
-            Color.init(hex: 0xe2e2e6)
+            Color.init(.systemGroupedBackground)
                 .ignoresSafeArea()
 
             ScrollView {
-                header
-                
-                Image("Profile")
-                    .resizable()
-                    .frame(
-                        maxWidth: maxProfileWidth,
-                        maxHeight: maxProfileWidth)
-                    .clipShape(Circle())
-                
-                let info = viewModel.getLevelInfo()
-                ProgressBarView(current: info.currentLevelXP, maximum: info.nextLevelXP, level: info.level)
-                    .padding(.bottom, 8)
-                
-                Text("Выполненно \(viewModel.nCurrentAchs) из \(viewModel.nTotalAchs) достижений")
-                    .font(.system(size: 32, weight: .bold))
-                    .foregroundColor(.primary)
-                    .multilineTextAlignment(.center)
+                VStack(spacing: 20) {
+                    header
+                    
+                    Image("Profile")
+                        .resizable()
+                        .frame(
+                            maxWidth: maxProfileWidth,
+                            maxHeight: maxProfileWidth)
+                        .clipShape(Circle())
+                    
+                    let info = viewModel.getLevelInfo()
+                    ProgressBarView(current: info.currentLevelXP, maximum: info.nextLevelXP, level: info.level)
+                        .padding(.bottom, 8)
+                    
+                    Text("Выполненно \(viewModel.nCurrentAchs) из \(viewModel.nTotalAchs) достижений")
+                        .font(.system(size: 32, weight: .bold))
+                        .foregroundColor(.primary)
+                        .multilineTextAlignment(.center)
 
-                VStack {
-                    ForEach(AchievementsStorage.shared.achs) { achievement in
-                        AchievementView(achievement: achievement)
-                        Divider()
+                    VStack {
+                        ForEach(AchievementsStorage.shared.achs) { achievement in
+                            AchievementView(achievement: achievement)
+                            Divider()
+                        }
                     }
                 }
-                .padding(16)
+                .padding(.horizontal, 16)
+                .padding(.top, 16)
+                .padding(.bottom, 32)
             }
             .padding(.horizontal, 16)
+            .scrollIndicators(.hidden)
+            .scrollContentBackground(.hidden)
+            .background(Color(.systemGroupedBackground))
         }
     }
     
